e6d1dd4ddd9c1f8c95f86d2704115d8b35016811
hariom-ojha23 <hariomojha2301@gmail.com>
2026-07-21 08:26:10 +0530

    feat: working on landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,8 +8,10 @@ import { Dashboard } from './feature/dashboard/dashboard'
 import { AuthGuard } from './core/guards/auth.guard'
 import { onboardingGuard } from './core/guards/onboarding.guard'
 import { GuestGuard } from './core/guards/guest.guard'
+import { Landing } from './feature/landing/landing'
 
 export const routes: Routes = [
+  { path: '', component: Landing },
   {
     path: '',
     component: AuthLayout,
